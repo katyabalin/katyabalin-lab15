d5602afd1ff829131b48a18c6220fbbbe22686e8
katyabalin <katya.balin@gmail.com>
2025-04-20 17:47:20 -0400

    added comments

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,7 +1,10 @@
 public class EnigmaGUI {
     public static void main(String[] arg) {
+        // set up the main gui window for the enigma machine
         EnigmaFrame ef = new EnigmaFrame();
+        // give the frame an internal name
         ef.setName("EnigmaGUI");
+        // make the window appear on the screen
         ef.setVisible(true);
     }
 }
